3b48c1379937a3e01ae58f8a52cae0e8444ba335
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-03 18:33:46 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+
+  attr_reader :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(info)
+    knowledge << info
+  end
+
+
 
 end
